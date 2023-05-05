6401ff6603fcc5ebbcbab5e9c9c76ffab5e503c9
Shagun Panghal <55479654+panghalshagun@users.noreply.github.com>
2023-05-05 18:48:12 +0530

    Update prob1.m

--- prob1.m
+++ prob1.m
@@ -1,31 +1,31 @@
-% clear all;
-% L=12;
-% x=linspace(0.05,1,L);
-% M=10;
-% u=rand(1,M);
-% w=rand(1,M);
-% for i=1:L
-%     for j=1:M
-%         N(i,j)=tanh(-x(i)*u(j)-w(j));
-%     end
-% end
-% for i=1:L
-%     for j=1:M
-%         mat(i,j)=u(j)^2*(-2)*(sech(-x(i)*u(j)-w(j)))^2*N(i,j)+u(j)*(tanh(u(j)*x(i)+w(j))^2-1)/x(i);
-%     end
-% end
-% for j=1:M
-%     mat(L+1,j)=u(j)*(tanh(w(j))^2-1);
-%     mat(L+2,j)=tanh(-u(j)-w(j));
-% end
-% rhs=zeros(L+2,1);
-% for i=1:L
-%     rhs(i)=-cos(x(i))-sin(x(i))/x(i);
-% end
-% rhs(L+1)=0;
-% rhs(L+2)=cos(1);
-% matt=pinv(mat);
-%   v=matt*rhs;
+ clear all;
+ L=12;
+ x=linspace(0.05,1,L);
+ M=10;
+ u=rand(1,M);
+ w=rand(1,M);
+ for i=1:L
+     for j=1:M
+         N(i,j)=tanh(-x(i)*u(j)-w(j));
+     end
+ end
+ for i=1:L
+     for j=1:M
+         mat(i,j)=u(j)^2*(-2)*(sech(-x(i)*u(j)-w(j)))^2*N(i,j)+u(j)*(tanh(u(j)*x(i)+w(j))^2-1)/x(i);
+     end
+ end
+ for j=1:M
+     mat(L+1,j)=u(j)*(tanh(w(j))^2-1);
+     mat(L+2,j)=tanh(-u(j)-w(j));
+ end
+ rhs=zeros(L+2,1);
+ for i=1:L
+     rhs(i)=-cos(x(i))-sin(x(i))/x(i);
+ end
+ rhs(L+1)=0;
+ rhs(L+2)=cos(1);
+ matt=pinv(mat);
+   v=matt*rhs;
 % % y=1;
 % % while y>0
 % %     y=input('enter x\n');
@@ -58,12 +58,12 @@ error=zeros(1,200);
 error(i)=answ(i)-act(i);
     end
     end
-% rms=rms+sqrt(error(i)^2);
-% rms=rms/200;
-%      end
-%  hh=plot(yy,error,'r-');
-%  xlabel('x','fontweight','bold','fontsize',16);
-%  ylabel('error in solution')
-% set(hh,'linewidth',1.5);
-%  set(gcf,'Color','w')
+ rms=rms+sqrt(error(i)^2);
+ rms=rms/200;
+      end
+  hh=plot(yy,error,'r-');
+  xlabel('x','fontweight','bold','fontsize',16);
+  ylabel('error in solution')
+ set(hh,'linewidth',1.5);
+  set(gcf,'Color','w')
 rms(error)
